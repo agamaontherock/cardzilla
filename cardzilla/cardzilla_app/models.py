ce8f69192650b7fb0b6848edb990dedeaa5fbc32
Victor Sklyar <agamaontherock@gmail.com>
2025-01-13 11:37:09 +0000

    cardset model

--- cardzilla/cardzilla_app/models.py
+++ cardzilla/cardzilla_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class CardSet(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    editors = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cardsets_editable')
+    viewers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cardsets_viewable')
+    
+    def __str__(self):
+        return self.name
